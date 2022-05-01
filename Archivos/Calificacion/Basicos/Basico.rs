5a5ee3e99c987c5814b7d0a56539acc31bb0f4f5
Abraham Solórzano <abraham81423@gmail.com>
2022-05-01 14:45:35 -0600

    [UPDATE] Archivo Basico

--- Archivos/Calificacion/Basicos/Basico.rs
+++ Archivos/Calificacion/Basicos/Basico.rs
@@ -7,8 +7,8 @@ fn main() {
     
     let bo1: bool = false;
     let bol21: bool = !bo1;
-    let cad1: String = "imprimir";
-    let cad21: String = "cadena valida";
+    let cad1: String = "imprimir".to_string();
+    let cad21: String = "cadena valida".to_string();
     let letra1: char = 'c';
     
     
@@ -47,7 +47,7 @@ fn main() {
     
     if a == b && j == k || 14 != c {
         println!(">>>>>> Esto debería de imprimirse");
-    } else {
+    }else{
         println!(">>>>>> Esto no debería de imprimirse");
     }
     
@@ -82,6 +82,46 @@ fn main() {
     let abs2:f64 = 10.0;
     let raiz1:i64 = 9;
     let raiz2:f64 = 100.0;
+     
+    
+    println!("");
+    println!("*************PRUEBA DE NATIVAS");
+    println!(" valor de b: {}",b);
+    
+    println!(" valor absoluto1: {}",abs1.abs());
+    println!(" valor absoluto2: {}",abs2.abs());
+    // println!(" valor raiz1: {}",(raiz1 as f64).sqrt());
+    // println!(" valor raiz2: {}",raiz2.sqrt());
     
     
 }
+
+/*
+
+----------------------
+----ARCHIVO BASICO----
+----------------------
+El valor de val11 es:              214
+El valor de val21 es:              412
+El valor de val31 es:              1439
+El resultado de la operación es:  1439
+El valor de bo1 es:                false
+El valor de cad1 es:               imprimir
+El valor de cad21 es:               cadena valida
+El valor de letra1 es:             c
+El valor de bol21:            true
+
+
+
+>>>>>> Esto debería de imprimirse
+>>>>>> Esto debería de imprimirse
+>>>>>> En este lugar deberia de entrar :)
+>>>>> numeroPar ingreso a if falso, 15 no es par
+
+*************PRUEBA DE NATIVAS
+ valor de b: 100
+ valor absoluto1: 4
+ valor absoluto2: 10
+ valor raiz1: 3
+ valor raiz2: 10
+*/
